9f1731802342d027c0f39ce87c06f869a737d376
Eric Gibby <eric.gibby@imaginelearning.com>
2021-03-11 10:36:39 -0700

    Fixed coverage path ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	coveragePathIgnorePatterns: [
 		'<rootDir>/node_modules/',
 		'<rootDir>/src/index.ts',
-		'<rootDir>/src/test/',
+		'<rootDir>/src/models/',
 	],
 	moduleNameMapper: {
 		// Need to map to the UMD build of ky for tests, since Jest can't properly handle ES modules
