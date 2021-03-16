514ebfa6dfe02d360dea61200617fe41883bbf9f
Eric Gibby <eric.gibby@imaginelearning.com>
2021-03-16 15:23:37 -0600

    Don't throw validation error if no schema defined

--- src/validate.ts
+++ src/validate.ts
@@ -7,9 +7,15 @@ function getSchema<T extends Event>(event: T) {
 	return schemas[context];
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function validate<T extends Event>(event: T, schema?: Record<string, any>) {
-	const { errors, valid } = validateSchema(event, schema ?? getSchema(event));
+export function validate<T extends Event>(
+	event: T,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	schema: Record<string, any> = getSchema(event)
+) {
+	if (!schema) {
+		return;
+	}
+	const { errors, valid } = validateSchema(event, schema);
 	if (!valid) {
 		throw errors.map((error) => error.stack);
 	}
